2c8263cd00c81cdd3ceeaa0f93c4393021e45213
Nguyen Anh Quan <anhq46724@gmail.com>
2024-07-27 10:32:00 +0700

    end post lean

--- Compose_posts/app/src/main/java/com/example/post/MainActivity.kt
+++ Compose_posts/app/src/main/java/com/example/post/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.post.ui.theme.PostTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +28,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PostTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeActiveApp()
                 }
             }
         }
@@ -31,17 +39,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeActiveApp(){
+    ArticleCard(
+        Title = stringResource(R.string.Compose_Tile),
+        BodySort = stringResource(R.string.Compose_Sort),
+        BodyLong = stringResource(R.string.Compose_Long),
+        ImgPath = painterResource(R.drawable.bg_compose_background))
+}
+
+@Composable
+fun ArticleCard(
+    Title: String,
+    BodySort: String,
+    BodyLong: String,
+    ImgPath: Painter,
+    modifier: Modifier = Modifier,
+){
+    Column(modifier = modifier) {
+        Image(painter = ImgPath, contentDescription = stringResource(R.string.imgHeader))
+        Text(
+            text = Title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = BodySort,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = BodyLong,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposePostsPreview(){
     PostTheme {
-        Greeting("Android")
+        ComposeActiveApp()
     }
 }
